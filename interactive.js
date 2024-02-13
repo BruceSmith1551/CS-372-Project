9f869021beaa8acf9f3eb37fbb178f27c77288fa
Bruce Smith <brucesmith1551@gmail.com>
2024-02-12 22:21:57 -0900

    added basic TO DO list to interactive.js

--- interactive.js
+++ interactive.js
@@ -5,7 +5,19 @@
 // To use Express, install it via 'npm install express'
 // Link to Express documentation: https://expressjs.com/en/5x/api.html
 // 
-// Link to Youtube tutorial: https://www.youtube.com/watch?v=Oe421EPjeBE&ab_channel=freeCodeCamp.org     GO TO: 4:51:50
+// Link to Express tutorial: https://www.youtube.com/watch?v=Oe421EPjeBE&ab_channel=freeCodeCamp.org     GO TO: 4:51:50
+//
+// TO DO:
+// - Handle input from HTML page
+//   - Use 'app.post' to recieve input data
+//     - Here we take data to either store/compare/process data from HTML page 
+//   - Will need app.use to parse JSON and URL-encoded bodies that is inputted
+//
+// - When receiving sign in input, add or compare to existing data in 'user_data.txt'
+//   - Update page to display error/success to client
+//     - e.g: "User successfully created" "User already exists"
+//   
+// - In response to successful login input, serve a new HTML page to HTML form
 
 const express = require('express');
 const path = require('path'); 
@@ -13,9 +25,7 @@ const path = require('path');
 const app = express();
 const PORT = 8080; // Easier to change if need be
 
-// Allows express to access files in current directory (known as serving)
-//app.use(express.static('public'));
-
+// Essentially 'serving' the HTML form
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'login.html'));
 });
